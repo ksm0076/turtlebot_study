d6a7fc38e7be199aea542e2a8feec97aef03e368
KSM <ksm0076@gmail.com>
2024-11-20 00:52:23 +0900

    Modify print log

--- my_pkg/measure_distance.py
+++ my_pkg/measure_distance.py
@@ -4,6 +4,7 @@ from rclpy.node import Node
 from nav_msgs.msg import Odometry
 import math
 from std_msgs.msg import String
+import time
 
 class DistanceTracker(Node):
     def __init__(self):
@@ -25,7 +26,12 @@ class DistanceTracker(Node):
         self.prev_y = None
         self.total_distance = 0.0
         self.is_measuring_distance = False
-        
+        self.start_time = None
+        self.end_time = None
+        self.speed = None
+        self.running_time = None
+        self.minute = None
+        self.second = None
         self.create_timer(1, self.show_distance)
         
     def odom_callback(self, msg):
@@ -37,7 +43,7 @@ class DistanceTracker(Node):
                 self.total_distance += distance
 
                 
-
+            self.speed = (self.total_distance*0.001) / ((time.time()-self.start_time)*(1/3600))
             self.prev_x = current_x
             self.prev_y = current_y
         
@@ -53,15 +59,35 @@ class DistanceTracker(Node):
         self.prev_x = None
         self.prev_y = None
         self.total_distance = 0.0
+        self.start_time = time.time()
+        self.minute = 0
+        self.second = 0
         print("Distance measurement START!!")
         
     def stop_distance_measurement(self):
         self.is_measuring_distance = False
-        print(f"Distance measurement STOP, Total distance : {self.total_distance:.2f} m")
-    
+        self.end_time = time.time()
+        print(f"Distance measurement STOP")
+        print(f"Total distance : {self.total_distance:.2f} m")
+        print(f"Average speed : {self.speed:.2f} Km/h")
+        print(f"Running time : {str(self.minute).zfill(2)}:{str(self.second).zfill(2)}")
+
     def show_distance(self):
         if self.is_measuring_distance:
-            self.get_logger().info(f'Traveled distance: {self.total_distance:.2f} m')
+            self.running_time = time.time()-self.start_time
+            
+            self.minute = int(self.running_time // 60)
+            self.second = int(self.running_time % 60)
+            # self.get_logger().info(f'Distance: {self.total_distance:.2f} m')
+            # self.get_logger().info(f'Average Speed: {self.speed:.2f} km/h')
+            # self.get_logger().info(f'Time: {str(self.minute).zfill(2)}:{str(self.second).zfill(2)}')
+            
+            
+            print(f'Distance: {self.total_distance:.2f} m')
+            print(f'Average Speed: {self.speed:.2f} km/h')
+            print(f'Time: {str(self.minute).zfill(2)}:{str(self.second).zfill(2)}')
+            print("------------------------------")
+            
         
 def main(args=None):
     rclpy.init(args=args)
